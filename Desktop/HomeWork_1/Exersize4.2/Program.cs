5dd68babcf000e9e358f13382610d23d0a00125a
Алёна Катанская <alyona.katanskaya@gmail.com>
2022-12-21 02:17:06 +0300

    Exersize 4.2

---
+++ Desktop/HomeWork_1/Exersize4.2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.ReadLine();
+
+int a = 44;
+int b = 5;
+int c = 78;
+
+int max = a;
+if (b > a)
+    max = b;
+if (c > b)
+    max = c;
+if (c > a)
+    max = c;
+
+Console.WriteLine(max);
