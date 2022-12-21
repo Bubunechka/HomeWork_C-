6b2459f2f606ca5b14716fe29c11ce91ce68a724
Алёна Катанская <alyona.katanskaya@gmail.com>
2022-12-21 03:11:06 +0300

    Exersize 6.1

---
+++ Desktop/HomeWork_1/Exersize6.1/Program.cs
@@ -0,0 +1,15 @@
+﻿//bool a;
+//a++;
+
+bool a = Random;
+bool b = 2.0;
+
+Console.WriteLine(a);
+
+//int v;
+a % b = v;
+if (v != 0)
+    Console.WriteLine("False");
+
+if (v = 0)
+    Console.WriteLine("True");
