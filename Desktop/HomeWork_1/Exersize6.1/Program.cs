44f4c9ac1d4fe937aba2ddba82823a948a8af54a
Bubunechka <110813295+Bubunechka@users.noreply.github.com>
2022-12-21 12:23:47 +0300

    update Exersize6.1

--- Desktop/HomeWork_1/Exersize6.1/Program.cs
+++ Desktop/HomeWork_1/Exersize6.1/Program.cs
@@ -1,15 +1,33 @@
 ﻿//bool a;
 //a++;
 
-bool a = Random;
-bool b = 2.0;
+//bool a = Random;
+//bool b = 2.0;
 
-Console.WriteLine(a);
+//Console.WriteLine(a);
 
 //int v;
-a % b = v;
-if (v != 0)
-    Console.WriteLine("False");
+//a % b = v;
+//if (v != 0)
+//    Console.WriteLine("False");
 
-if (v = 0)
-    Console.WriteLine("True");
+//if (v = 0)
+//    Console.WriteLine("True");
+
+int i;
+
+Console.Write("Введите число : ");
+
+ i = int.Parse(Console.ReadLine());
+
+ if (i % 2 == 0)
+ {
+     Console.Write("Введенное число является четным");
+      Console.Read();
+ }
+
+else
+{
+    Console.Write("Введенное число нечетное");
+    Console.Read();
+}
